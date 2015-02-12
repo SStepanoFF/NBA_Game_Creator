4dd4fdab64a7246f0bef40a0d894667f549b20c0
sergii.stepanov <crfkjkfp>
2015-02-12 15:32:29 +0200

    SFTP connect

--- src/main/java/GameCreator.java
+++ src/main/java/GameCreator.java
@@ -34,7 +34,7 @@ public class GameCreator {
         writeToFile("<Msg_file LeagueID=\"00\" League=\"NBA\" Season=\"2014-15\" SeasonType=\"Regular Season\">\n" +
                 "  <Game Number=\"0\">\n" +
                 "\t<Msg_game_info>\n" +
-                "\t\t<Game_info Game_id=\""+ gameID+"\" Game_date=\""+takeCurrentDate("MM/dd/YYYY")+"\" Game_time=\"10:30 AM\" Home_time=\"10:30 AM\" Visitor_time=\"10:30 AM\" Arena_name=\"Forbes Road Pavilion\" Location=\"Braintree, MA\" PPD_Status=\"I\" Game_date_UTC=\""+takeCurrentDate("YYYY-MM-dd")+"\" Game_time_UTC=\"15:30\" />\n" +
+                "\t\t<Game_info Game_id=\""+ gameID+"\" Game_date=\""+takeCurrentDate("MM/dd/YYYY")+"\" Game_time=\"05:30 AM\" Home_time=\"05:30 AM\" Visitor_time=\"05:30 AM\" Arena_name=\"Forbes Road Pavilion\" Location=\"Braintree, MA\" PPD_Status=\"I\" Game_date_UTC=\""+takeCurrentDate("YYYY-MM-dd")+"\" Game_time_UTC=\"10:30\" />\n" +
                 "\t\t<Home_team Team_id=\"7818498118\" Team_city=\"Braintree\" Team_name=\"Quality\" Team_abr=\"WA_HOME\" />\n" +
                 "\t\t<Visitor_team Team_id=\"7818498119\" Team_city=\"\" Team_name=\"Assurance\" Team_abr=\"WA_AWAY\" />\n" +
                 "\t\t<TV_Info Home=\"NA_QA\" Away=\"NA_QA2\" Natnl=\"NA_QA3\" Canadian=\"NA_QA4\" />\n" +
